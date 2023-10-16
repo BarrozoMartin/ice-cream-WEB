70c9665f9d933cbbec0b2c3fce20a7e0c178b7aa
Martin Barrozo <124919567+BarrozoMartin@users.noreply.github.com>
2023-10-16 16:36:34 -0300

    Upgrades

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const showFormButton = document.getElementById("showFormButton");
+
+showFormButton.addEventListener("click", function () {
+    window.location.href = "./pages/formulario.html";
+});
+
+    const btnBack = document.getElementById("btnBack");
+
+    btnBack.addEventListener("click", function () {
+        window.location.href = "../index.html#top";
+    });
+
+
